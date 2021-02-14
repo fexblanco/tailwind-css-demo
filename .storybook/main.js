bbf538693d30c58f363d785f1798f5202d86553e
Félix Blanco <fexblanco@gmail.com>
2021-02-14 14:17:55 +0100

    Funcionando

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,23 @@
+const { merge } = require('webpack-merge');
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: (config) => {
+    return merge(config, {
+      module: {
+        rules: [
+          {
+            test: /\.hbs$/,
+            use: 'handlebars-loader'
+        },
+        ]
+      }
+    });
+  },
 }
---
+++ src/components/button/button.stories.js
@@ -0,0 +1,9 @@
+export default {
+  title: `Atoms/Button`,
+  decorators: [],
+  parameters: {}
+}
+
+import template from './button.hbs';
+
+export const Default = () => template();
